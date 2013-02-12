7d2e1d0d6259a9a4248afb0ee037dd288ff31c3d
Thanasis Polychronakis <thanpolas@gmail.com>
2013-02-12 02:27:48 +0200

    created test case for grunt tasks

---
+++ test/fixtures/case/js/app/router.js
@@ -0,0 +1,14 @@
+goog.provide('Todos.Router');
+
+goog.require('Todos.ctrls.Todos');
+goog.require('Todos.views.Items');
+goog.require('Todos.helper');
+
+/**
+* Todos Router
+*
+* Defined routes represent filters according to specs
+*
+* @returns Class
+*/
+Todos.Router = Ember.Router.extend({});
---
+++ test/fixtures/case/js/lib/deppy/deppy.js
@@ -0,0 +1,9 @@
+/**
+ * This file is prepended on the final built in order to perform
+ * hacks, stubs and mocks for the closure library and its flow.
+ *
+ * Look in grunt config.
+ */
+
+// do not allow closure to ask for deps.js
+CLOSURE_NO_DEPS = true;
