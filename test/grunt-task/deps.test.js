1e8e9455e5b72d2d7ac121b2a5c751c6d8d6cd1e
Thanasis Polychronakis <thanpolas@gmail.com>
2013-02-17 00:48:19 +0200

    adjusted deps test

--- test/grunt-task/deps.test.js
+++ test/grunt-task/deps.test.js
@@ -27,6 +27,13 @@ describe('Grunt task :: dependency', function(){
     var actual = grunt.file.read(tmp + actualFile);
     var expected = grunt.file.read(fixtures + actualFile);
 
+    // cheat a little, first line is the depsWriter.py full path declaration.
+    // this breaks tests in each different setup.
+    //
+    // so, remove first line from both files
+    actual = actual.split('\n').splice(0,1).join('\n');
+    expected = expected.split('\n').splice(0,1).join('\n');
+
     assert.equal(actual, expected, 'task output should equal: ' + actualFile);
 
   });
